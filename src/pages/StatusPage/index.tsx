4af5e5bb672acd788f177f23310e2a31c6f52ceb
DEWEER Bertrand (EXT EXTIA) <PBDR10191@COMMUN.AD.SNCF.FR>
2020-04-29 15:57:37 +0200

    api down management

--- src/pages/StatusPage/index.tsx
+++ src/pages/StatusPage/index.tsx
@@ -3,69 +3,85 @@ import { PageStatus, StatusRow } from '../../components';
 import { begin } from '../../services';
 import { Api, ApiWithResponse, Response } from '../../typings/Api';
 import { Container } from './StatusPage.elements';
-
+ 
 interface StatusPageProps {
-  /** An array of API objects */
-  apis: Api[];
-  /** The interval at which to call the APIs in milliseconds */
-  interval?: number;
+ /** An array of API objects */
+ apis: Api[];
+ /** The interval at which to call the APIs in milliseconds */
+ interval?: number;
 }
-
+ 
 interface ApiWithError extends ApiWithResponse {
-  hasError: boolean;
+ hasError: boolean;
 }
-
+ 
 interface StatusPageState {
-  apis: ApiWithError[];
-  hasError: boolean;
+ apis: ApiWithError[];
+ hasError: boolean;
 }
-
+ 
 class StatusPage extends Component<StatusPageProps> {
-  public state: StatusPageState = {
-    apis: [],
-    hasError: false,
-  };
-
-  public handleError = (api: Api, response: Response) => {
-    const apis = [...this.state.apis];
-    const index = apis.findIndex(stateApi => stateApi.api === api);
-    apis[index].hasError = true;
-    apis[index].response = response;
-
-    this.setState(
-      {
-        apis,
-        hasError: true
-      }
-    );
-  };
-
-  public componentDidMount = async () => {
-    const apis = this.props.apis.map(api => ({ api, response: null, hasError: false }));
-
-    this.setState({
-      apis,
-      hasError: false,
+ public state: StatusPageState = {
+   apis: [],
+   hasError: false,
+ };
+ 
+ 
+ public handleError = (api: Api, response: Response) => {
+   const apis = [...this.state.apis];
+   const index = apis.findIndex(stateApi => stateApi.api === api);
+   apis[index].response = response;
+  
+   if (response.code == 200) {
+    apis[index].hasError = false;
+    let hasError = false;
+    apis.forEach(api => {
+     if (api.hasError) hasError = true;
+   });
+ 
+   this.setState({
+     apis,
+     hasError: hasError,
     });
-
-    await begin(this.props.apis, this.handleError, this.props.interval, undefined);
-  };
-
-  public render() {
-    return (
-      <Container>
-        <PageStatus hasError={this.state.hasError} />
-        {this.state.apis.map((api, i) => (
-          <StatusRow
-            key={api.api.name + api.api.endpoint}
-            name={api.api.name}
-            hasError={api.hasError}
-            className={i === 0 ? 'first' : ''}
-          />
-        ))}
-      </Container>
-    );
-  }
+   } 
+   else {
+    apis[index].hasError = true;
+    
+    this.setState({
+       apis,
+       hasError: true,
+     });
+   }
+ };
+ 
+ public componentDidMount = async () => {
+   const apis = this.props.apis.map(api => ({ api, response: null, hasError: false }));
+ 
+   this.setState({
+     apis,
+     hasError: false,
+   });
+ 
+   await begin(this.props.apis, this.handleError, this.props.interval, undefined);
+ };
+ 
+ public render() {
+   return (
+     <Container>
+       <PageStatus hasError={this.state.hasError} />
+       {this.state.apis.map((api, i) => (
+         <StatusRow
+           key={api.api.name + api.api.endpoint}
+           name={api.api.name}
+           hasError={api.hasError}
+           className={i === 0 ? 'first' : ''}
+         />
+       ))}
+     </Container>
+   );
+ }
 }
-
+ 
 export default StatusPage;
+ 
+
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,51 +1,36 @@
 import { Api, Response as ApiResponse } from '../typings/Api';
 import { Handler } from '../typings/Handler';
-
+ 
 /** Begins the chain of API calls */
-export const begin = async (
-  apis: Api[],
-  onError: Handler,
-  interval?: number,
-  onResponse?: Handler
-) => {
-  const currentInterval = interval ? interval : 30000;
-
-  apis.forEach(async a => {
-    await makeCall(a, onError, onResponse || undefined);
-  });
-
-  /** Set the timeout and do it again */
-  setTimeout(
-    async () => await begin(apis, onError, currentInterval, onResponse || undefined),
-    currentInterval
-  );
+export const begin = async (apis: Api[], onResponse: Handler, interval?: number) => {
+ const currentInterval = interval ? interval : 30000;
+ 
+ apis.forEach(async a => {
+   await makeCall(a, onResponse);
+ });
+ 
+ /** Set the timeout and do it again */
+ setTimeout(async () => await begin(apis, onResponse, currentInterval), currentInterval);
 };
-
+ 
 /** Make the actual fetch and return a Response object */
-const makeCall = async (api: Api, onError: Handler, onResponse?: Handler) => {
-  await fetch(api.endpoint)
-    .then(apiResponse => handleResponse(api, apiResponse, onError, onResponse || undefined))
-    // tslint:disable-next-line
-    .catch(error => console.log('unhandled', error));
+const makeCall = async (api: Api, onResponse: Handler) => {
+ await fetch(api.endpoint)
+   .then(apiResponse => handleResponse(api, apiResponse, onResponse))
+   // tslint:disable-next-line
+   .catch(error => handleResponse(api, error, onResponse));
 };
-
+ 
 /**
- * Handles the Response
- */
-const handleResponse = (api: Api, response: Response, onError: Handler, onResponse?: Handler) => {
-  const apiResponse: ApiResponse = {
-    code: response.status,
-    message: response.statusText,
-  };
-
-  if (!response.ok) {
-    onError(api, apiResponse);
-    return;
-  }
-
-  if (onResponse) {
-    onResponse(api, apiResponse);
-  }
+* Handles the Response
+*/
+const handleResponse = (api: Api, response: Response, onResponse: Handler) => {
+ const apiResponse: ApiResponse = {
+   code: response.status,
+   message: response.statusText,
+ };
+ 
+ onResponse(api, apiResponse);
 };
-
+ 
 export default { begin, makeCall, handleResponse };
